3cebee308cd704cbfebb95b5de46517891432414
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2018-08-05 17:44:15 +0100

    finished tutorial part two

---
+++ tutorial-part-two/src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography";
+// import bootstrapTheme from "typography-theme-bootstrap"
+// import lawtonTheme from "typography-theme-lawton";
+import lincolnTheme from 'typography-theme-lincoln';
+
+const typography = new Typography(lincolnTheme);
+
+export default typography;
